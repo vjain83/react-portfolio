ea761ba5c8a2e2400bbe097005596aa27a719c72
Varsha Jain <csvarshajain@gmail.com>
2022-03-03 22:05:12 -0500

    added updated resume

--- src/componenets/Resume/index.js
+++ src/componenets/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
                 <h1 className="page-header">My Resume</h1>
             </div>
             <div className="bottom-spacing">
-                <a href={require("../../assets/images/Resume-Varsha.pdf")} download>
+                <a href={require("../../assets/images/Resume-Varsha.docx")} download>
                     <h4>Download my Resume</h4>
                 </a>
             </div>
